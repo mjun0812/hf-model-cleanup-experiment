7c1f4c900f593703c671fea10e99dd41ad951414
Junya Morioka <mjun@mjunya.com>
2025-01-11 22:40:22 +0900

    fix figures

--- run.sh
+++ run.sh
@@ -9,9 +9,9 @@ for MEMORY_SIZE in ${MEMORY_SIZES[@]}; do
     docker run -it --rm --memory=$MEMORY_SIZE -v $(pwd):/app memory-profiler python /app/3.py
     docker run -it --rm --memory=$MEMORY_SIZE -v $(pwd):/app memory-profiler python /app/4.py
 
-    docker run -it --rm -v $(pwd):/app memory-profiler python /app/plot_all.py
-
     mkdir -p figs/$MEMORY_SIZE csv/$MEMORY_SIZE
-    mv figs/*.png figs/$MEMORY_SIZE
-    mv csv/*.csv csv/$MEMORY_SIZE
+    mv -f figs/*.png figs/$MEMORY_SIZE
+    mv -f csv/*.csv csv/$MEMORY_SIZE
+
+    docker run -it --rm -v $(pwd):/app memory-profiler python /app/plot_all.py csv/$MEMORY_SIZE
 done
